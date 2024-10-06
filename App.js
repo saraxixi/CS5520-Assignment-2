f911197679338890d33a0fa998d7482b349daa3e
saraxixi <saraxi.xixi@gmail.com>
2024-10-05 22:31:55 -0700

    Add Diet, Settings, and Activities screens
    This commit adds three new screens: Diet, Settings, and Activities. Each screen contains a basic layout with a text component displaying the screen name. The screens are imported and rendered in the App component using the react-navigation library. The necessary dependencies for react-navigation, including @react-navigation/bottom-tabs, @react-navigation/native, and @react-navigation/stack, are also added to the package.json file.

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
+import Activities from './screens/Activities';
+import Diet from './screens/Diet';
+import Settings from './screens/Settings';
+
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen 
+          name="Activities" 
+          component={Activities} 
+          options={{
+            tabBarLabel: 'Activities',
+            tabBarIcon: ({ color }) => (
+              <FontAwesome5 name="running" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Diet" 
+          component={Diet}
+          options={{
+            tabBarLabel: 'Diet',
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="fastfood" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings" 
+          component={Settings}
+          options={{
+            tabBarLabel: 'Settings',
+            tabBarIcon: ({ color }) => (
+              <SimpleLineIcons name="settings" size={24} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+
   );
 }
 
