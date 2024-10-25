4647882cfee3def9dddb49eaa8e701ceb567e5de
saraxixi <saraxi.xixi@gmail.com>
2024-10-24 18:33:01 -0700

    Refactor Settings.js to use PressableButton for theme toggle

--- screens/Settings.js
+++ screens/Settings.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, Button } from 'react-native'
 import React, {useContext} from 'react'
 import { ThemeContext } from '../components/ThemeContext'
 import Styles, { commonStyles } from '../components/Styles'
+import PressableButton from '../components/PressableButton'
 
 export default function Settings() {
   const { theme, toggleTheme } = useContext(ThemeContext)
@@ -9,7 +10,12 @@ export default function Settings() {
   return (
     <View style={theme === 'light' ? commonStyles.lightContainer : commonStyles.darkContainer}>
       <View style={commonStyles.themeButtonContainer}>
-      <Button title="Toggle Theme" onPress={toggleTheme} />
+        <PressableButton
+          pressedFunction={toggleTheme}
+          componentStyles={{backgroundColor: '#3b3c7e', padding: 10}}>
+          <Text style={commonStyles.buttonText}>Toggle Theme</Text>
+        </PressableButton>
+      {/* <Button title="Toggle Theme" onPress={toggleTheme} /> */}
       </View>
     </View>
   )
